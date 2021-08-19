d179ea6a09a08ca2d1073cba958d9c62ba55f1a9
rafams30 <rafaelms3096@gmail.com>
2021-08-18 21:39:05 -0300

    inicio alteração cadastro

--- java/exercicio/src/main/java/br/com/gerenciador/exercicio/controller/UsuarioController.java
+++ java/exercicio/src/main/java/br/com/gerenciador/exercicio/controller/UsuarioController.java
@@ -3,17 +3,17 @@ package br.com.gerenciador.exercicio.controller;
 import br.com.gerenciador.exercicio.domain.dto.UsuarioRequest;
 import br.com.gerenciador.exercicio.domain.model.Usuario;
 import br.com.gerenciador.exercicio.service.UsuarioService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
 
+@Slf4j
+@CrossOrigin
 @RestController
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
@@ -23,6 +23,7 @@ public class UsuarioController {
 
     @PostMapping
     public ResponseEntity<Usuario> save(@RequestBody @Valid UsuarioRequest request, UriComponentsBuilder uriBuilder){
+        log.info("chamando save do usario controller: " + request);
         Usuario usuario = service.save(request);
         URI uri = uriBuilder.path("api/usuarios/{id}").buildAndExpand(usuario.getId()).toUri();
         return ResponseEntity.created(uri).body(usuario);
